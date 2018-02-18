7c25154c71f32734871c867647517212e160393f
Max Fulgoni <fulgonim@gmail.com>
2018-02-18 20:14:55 +0000

    added runServer and closeServer functions to server.js

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const jsonParser = bodyParser.json();
 const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
 //const morgan = require('morgan');
 
 // import the port and database URL from the config.js file
@@ -18,14 +20,46 @@ const router = require('./router');
 app.use('/blog-posts', router);
 //app.use(morgan('common'));
 
-//connect to mongo database
-mongoose.connect(DATABASE_URL);
+// declare a global 'server' object for runServer and closeServer functions
+let server;
 
+// connect to database and then start the server
+function runServer(databaseURL, port = PORT) {
 
-app.listen(PORT, () => {
-	console.log(`Your app is listening on ${PORT}`);
-})
+	return new Promise((resolve, reject) => {
+		mongoose.connect(databaseURL, err => {
+			if (err) {
+				return reject(err);
+			}
+			server = app.listen(port, () => {
+				console.log(`Your app is listening on port ${PORT}`);
+				resolve();
+			})
+				.on('error', err => {
+					mongoose.disconnect();
+					reject(err);
+				});
+		});
+	});
+}
 
+// close server and return a promise (for integration tests)
+function closeServer() {
+	return mongoose.disconnect().then(() => {
+		return new Promise((resolve, reject) => {
+			console.log('Closing server');
+			server.close(err => {
+				if (err) {
+					return reject(err);
+				}
+				resolve();
+			});
+		});
+	});
+}
 
+if (require.main === module) {
+	runServer(DATABASE_URL).catch(err => console.error(err));
+}
 
-
+module.exports = {app, runServer, closeServer};
