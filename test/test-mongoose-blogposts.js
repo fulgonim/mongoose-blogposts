77572addda81135fff76dd40d1f622d2344b7da0
Max Fulgoni <fulgonim@gmail.com>
2018-03-02 08:19:37 +0800

    initial test file commit by max

---
+++ test/test-mongoose-blogposts.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const mongoose = require('mongoose');
+const faker = require('faker');
+
+const expect = chai.expect;
+
