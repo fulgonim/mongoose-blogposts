ca590b26c7f89a04bf684f787f8ece7759891e8c
Max Fulgoni <fulgonim@gmail.com>
2018-03-02 10:45:07 +0800

    seed and tear down functions written in test file need to add actual CRUD tests

--- test/test-mongoose-blogposts.js
+++ test/test-mongoose-blogposts.js
@@ -14,30 +14,35 @@ const {TEST_DATABASE_URL} = require('../config');
 chai.use(chaiHTTP);
 
 function seedBlogpostData() {
+	console.info('seeding restaurant data');
+	const seedData = [];
+
+	for (let i = 1; i <= 10; i++) {
+		seedData.push(generateBlogpostData());
+	}
+
+	return Blogpost.insertMany(seedData);
 	//put "random" blogpost documents into the db
 	//use Faker library to generate placeholder values for title, content and author
-};
-
-function generateTitle() {
-	//generate a random titles for blogposts
-};
-
-function generateContent() {
-	//generate some random content for blogposts
-};
-
-function generateAuthor() {
-	//generate a random author name (first and last) for blogposts
-};
-
-
-function generateBlogpostData() {
-	//parent function for all "generate" functions
-	//used to create an object representing a blogpost for seed data for db
-	//or request.body data
+	//(generate these values using the "generate" fcns below)
 }
 
+//used to create an object representing a blogpost for seed data for db
+//or request.body data
+function generateBlogpostData() {
+	return {
+		title: faker.lorem.sentence(),
+		content: faker.lorem.paragraph(),
+		author: {firstName: faker.name.firstName(), 
+				lastName: faker.name.lastName()}
+	};
+}
 
+//delete the entire test db
+function tearDownDb() {
+	console.warn('Deleting database');
+	return mongoose.connection.dropDatabase();
+}
 
 
 
