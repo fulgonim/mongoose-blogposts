9745e51516dee89a8d1a573d67a939132146bfa5
Max Fulgoni <fulgonim@gmail.com>
2018-03-02 09:06:04 +0800

    function stubs for generating test data

--- test/test-mongoose-blogposts.js
+++ test/test-mongoose-blogposts.js
@@ -7,3 +7,38 @@ const faker = require('faker');
 
 const expect = chai.expect;
 
+const {Blogpost} = require('../models');
+const {app, runServer, closeServer} = require('../server');
+const {TEST_DATABASE_URL} = require('../config');
+
+chai.use(chaiHTTP);
+
+function seedBlogpostData() {
+	//put "random" blogpost documents into the db
+	//use Faker library to generate placeholder values for title, content and author
+};
+
+function generateTitle() {
+	//generate a random titles for blogposts
+};
+
+function generateContent() {
+	//generate some random content for blogposts
+};
+
+function generateAuthor() {
+	//generate a random author name (first and last) for blogposts
+};
+
+
+function generateBlogpostData() {
+	//parent function for all "generate" functions
+	//used to create an object representing a blogpost for seed data for db
+	//or request.body data
+}
+
+
+
+
+
+
